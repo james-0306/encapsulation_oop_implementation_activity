8bb8cb3e47b1d768da63f37552026cb2931ce0cd
james malitan <jamesmalitan21@gmail.com>
2026-06-05 13:17:34 +0800

    changing the value of name, animal_type, and the age. it will stored the value of inputed data

--- pet_class/pet_class.py
+++ pet_class/pet_class.py
@@ -1,6 +1,25 @@
 class Pet:
     def __init__(self, name, animal_type, age):
+        self.name = " "
+        self.animal_type = " "
+        self.age = 0
+
+    # setter method
+    def set_name(self, name):
         self.name = name
+
+    def set_animal_type(self, animal_type):
         self.animal_type = animal_type
+
+    def set_age(self, age):
         self.age = age
 
+    # getter method
+    def get_name(self):
+        return self.name
+
+    def get_animal_type(self):
+        return self.animal_type
+
+    def get_age(self):
+        return self.age
