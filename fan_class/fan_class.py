4cf696538656017ae6e228e087dcd8c54722446f
james malitan <jamesmalitan21@gmail.com>
2026-06-05 11:44:26 +0800

    adding the property and setter of the speed

--- fan_class/fan_class.py
+++ fan_class/fan_class.py
@@ -3,4 +3,28 @@ class Fan:
     SLOW = 1
     MEDIUM = 2
     FAST = 3
+    def __init__(self, speed = SLOW, radius = 5, color = "blue", on = False):
+        self.__speed = speed
+        self.__radius = radius
+        self.__color = color
+        self.__on = on
+
+    @property
+    def speed(self):
+        return self.__speed
+
+    @speed.setter
+    def speed(self, new_speed):
+        if new_speed in (Fan.SLOW, Fan.MEDIUM, Fan.FAST):
+            self.__speed = new_speed
+        else:
+            raise ValueError("Invalid speed")
+
     
+
+
+
+
+
+
+
