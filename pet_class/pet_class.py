fdf02229fbf21d61e67935c3824eacb595ac652b
james malitan <jamesmalitan21@gmail.com>
2026-06-05 13:20:33 +0800

    testing commit again for a removal of excess files

--- pet_class/pet_class.py
+++ pet_class/pet_class.py
@@ -1,25 +1,25 @@
 class Pet:
     def __init__(self, name, animal_type, age):
-        self.name = " "
-        self.animal_type = " "
-        self.age = 0
+        self.__name = " "
+        self.__animal_type = " "
+        self.__age = 0
 
     # setter method
     def set_name(self, name):
-        self.name = name
+        self.__name = name
 
     def set_animal_type(self, animal_type):
-        self.animal_type = animal_type
+        self.__animal_type = animal_type
 
     def set_age(self, age):
-        self.age = age
+        self.__age = age
 
     # getter method
     def get_name(self):
-        return self.name
+        return self.__name
 
     def get_animal_type(self):
-        return self.animal_type
+        return self.__animal_type
 
     def get_age(self):
-        return self.age
+        return self.__age
